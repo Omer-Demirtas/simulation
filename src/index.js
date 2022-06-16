fb493b0a02d4ffae4d69ca655b29f6afc39c61d5
ME99480 <omer.demirtas@toyotatr.com>
2022-06-16 18:33:42 +0300

    finish router configuration.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+
+
+const HomePage = () => 
+{
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
+}
+
+export default HomePage;
